6250bc59fd5aba08cbdd96280ca45ab3a2c77e73
MichaelMFeldman <michael@infoscoutinc.com>
2016-02-15 11:18:59 -0800

    New improved approach: the decorator returns a class instance

--- boomerang/boomerang.py
+++ boomerang/boomerang.py
@@ -1,15 +1,79 @@
+import importlib
 from models import Job
 from celery.task import task
+from django.db import transaction
 
-def boomerang(function, *args, **kwargs):
-    name = function.__name__.replace("_", " ").capitalize()[:64]
-    job = Job.objects.create(name=name)
-    boomerang_task.delay(function, job, *args, **kwargs)
+def boomerang(function):
+    """
+    @param function: Function to be called in a celery task
+    @return: Boomerang: Instance whose methods (__call__, delay, apply_async) are called in code
+    """
+
+    class Boomerang:
+        def __init__(self):
+            # Store the original function so boomerang_task can call it
+            self.original_function = function
+
+        def __call__(self, *args, **kwargs):
+            """
+            As long as the function doesn't interact with its Job, it can be called
+            as a normal function.
+            """
+            return function(*args, **kwargs)
+
+        def delay(self, *args, **kwargs):
+            """
+            delay() is the celery shortcut for passing args/kwargs to apply_async,
+            so this is the same shortcut.
+            """
+            return self.apply_async(args=args, kwargs=kwargs)
+
+        def apply_async(self, args=None, kwargs=None, **c_kwargs):
+            """
+            @param args: Tuple of arguments to be passed to function
+            @param kwargs: Dict of arguments to be passed to function
+            @param **c_kwargs: Dict of arguments to be passed to celery (e.g. countdown)
+            @return: Job
+            """
+
+            with transaction.commit_on_success():
+                # Create the Job synchronously so it immediately appears in the admin site.
+                # Do this in a transaction to guarantee that it's committed in the db
+                # before it's used in a celery thread.
+                job = Job.create_with_fn_name(function)
+
+                # Pass the location and name of the function, so that when the task runs
+                # it imports the latest version of that function's code.
+                extra_info = (function.__module__, function.__name__, job.id)
+                args = extra_info + (args or ())
+                kwargs = kwargs or {}
+                c_kwargs = c_kwargs or {}
+                
+            boomerang_task.apply_async(args=args, kwargs=kwargs, **c_kwargs)
+            return job
+
+    return Boomerang()
+
 
 @task
-def boomerang_task(function, job, *args, **kwargs):
+def boomerang_task(module, name, job_id, *args, **kwargs):
+    """
+    @param module: String module path where the function is
+    @param name: String name of the function
+    @param job_id: Job id
+    @param args, kwargs: Passed to the function
+    """
+
     try:
-        function(job, *args, **kwargs)
+        # Reimport the function
+        module = importlib.import_module(module)
+        boomerang_instance = getattr(module, name)
+
+        job = Job.objects.get(id=job_id)
+        boomerang_instance.job = job
+
+        job.set_status("RUNNING")
+        boomerang_instance.original_function(job, *args, **kwargs)
         job.set_status("DONE")
     except:
         job.set_status("FAILED")
--- boomerang/models.py
+++ boomerang/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Job(models.Model):
 
     STATUS_CHOICES = (
+        ("NOTRUNNING", "Not yet running"),
         ("RUNNING", "Running"),
         ("DONE", "Done"),
         ("FAILED", "Failed"),
@@ -10,12 +11,21 @@ class Job(models.Model):
 
     name = models.CharField(max_length=64, default="Unnamed job")
     start_time = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=32, choices=STATUS_CHOICES, default="RUNNING")
+    status = models.CharField(max_length=32, choices=STATUS_CHOICES, default="NOTRUNNING")
     progress = models.PositiveIntegerField(default=0, verbose_name="Progress (Approximate)")
     goal = models.PositiveIntegerField(null=True, blank=True)
 
+
+    @classmethod
+    def create_with_fn_name(cls, function):
+        name = function.__name__.replace("_", " ").capitalize()
+        job = cls()
+        job.set_name(name)
+        return job
+
     def set_name(self, name):
-        self.name = name
+        max_len = self._meta.get_field('name').max_length
+        self.name = name[:max_len]
         self.save()
 
     def set_status(self, status):
