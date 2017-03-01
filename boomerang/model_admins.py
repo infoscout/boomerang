f1adf1faa52313d8faf8f572a0ffd2a85d3181b7
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-03-01 13:44:00 -0800

    CA-5649 Remove ability to edit jobs in admin

--- boomerang/model_admins.py
+++ boomerang/model_admins.py
@@ -4,9 +4,17 @@ from django.contrib import admin
 class JobAdmin(admin.ModelAdmin):
 
     list_display = ('id', 'name', 'start_time', 'elapsed_time', 'status', 'progress', 'goal',)
+    list_display_links = None
+    actions = None
 
     def has_add_permission(self, request, obj=None):
         return False
 
     def has_delete_permission(self, request, obj=None):
         return False
+
+    def save_model(self, request, obj, form, change):
+        """
+        ie. remove permission to edit the job
+        """
+        pass
