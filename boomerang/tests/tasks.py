c1d0db1154080728671ebaa791e67b48ab245c0c
Lucas Connors <lucas.revolutiontech@gmail.com>
2018-06-12 15:47:10 -0700

    PLAT-905 Add tests for goal size of 1 and failing Boomerang tasks

--- boomerang/tests/tasks.py
+++ boomerang/tests/tasks.py
@@ -6,4 +6,16 @@ class SimpleBoomerangTask(BoomerangTask):
     @staticmethod
     def perform_async(job, integers):
         for _ in integers:
-            job.increment_progress()
+            if job:
+                job.increment_progress()
+
+
+class FailingBoomerangTask(BoomerangTask):
+
+    @staticmethod
+    def perform_async(job, integers):
+        for i in integers:
+            if i == 5:
+                raise ValueError("Cannot handle value of 5")
+            if job:
+                job.increment_progress()
